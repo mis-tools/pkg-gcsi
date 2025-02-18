1c84e2b340e3302b35ff713e5829f17e67144e22
Christian P. V. Christoffersen <88783438+br-cpvc@users.noreply.github.com>
2025-02-18 10:17:40 +0100

    moved download of cmake outside of Dockerfile

--- scripts/build_using_docker.sh
+++ scripts/build_using_docker.sh
@@ -3,6 +3,10 @@ set -ex
 script_dir=$(dirname "$0")
 cd ${script_dir}/..
 
+if [[ ! -f scripts/docker_build_environment/cmake.tar.gz ]]; then
+wget https://github.com/Kitware/CMake/releases/download/v3.25.1/cmake-3.25.1-linux-x86_64.tar.gz -O scripts/docker_build_environment/cmake.tar.gz
+fi
+
 docker_img_name=build-env_`basename $(git remote show -n origin | grep Fetch | cut -d: -f2-) .git`
 echo "building docker image: ${docker_img_name}"
 docker build -t ${docker_img_name} scripts/docker_build_environment
